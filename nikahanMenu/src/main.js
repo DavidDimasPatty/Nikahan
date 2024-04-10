f1c90e6b344327f74e24273928b5b910c4cc78a8
unknown <daviddimas80@gmail.com>
2024-04-10 10:02:01 +0700

    isi content

--- nikahanMenu/src/main.js
+++ nikahanMenu/src/main.js
@@ -2,7 +2,7 @@ import './assets/main.css'
 import router from './router';
 import { createApp } from 'vue'
 import App from './App.vue'
-
+// import 'bootstrap/dist/css/bootstrap.css';
 
 createApp(App)
   .use(router)
--- nikahanMenu/src/views/HomePage.vue
+++ nikahanMenu/src/views/HomePage.vue
@@ -1,45 +1,247 @@
 <template>
   <div class="book">
     <div id="pages" class="pages">
-      <div class="page" v-for="(page, index) in pages" :key="index" @click="flipPage(index)">
-        <p v-if="index % 2 === 0">Open Me, <br>please!</p>
-        <p v-else>Hello there!</p>
+      <!-- page 1 -->
+      <div class="page">
+        <p>Open Me, <br />please!</p>
       </div>
+      <!-- page 2 -->
+      <div class="page"><p>Halo David, You're Invited</p></div>
+      <div class="page"><p>Pernikahan Farhan dan Risma</p></div>
+      <!-- page 3 -->
+      <div class="page">
+        <blockquote class="blockquote w-75">
+          <p class="mb-3">
+            "Mencintai dan dicintai adalah puncak kebahagiaan dan kekayaan.
+            Semoga kalian tidak pernah melupakan harta berharga ini sepanjang
+            hari bersama."
+          </p>
+          <footer class="blockquote-footer">
+            Someone famous in <cite title="Source Title">Bang Toyib</cite>
+          </footer>
+        </blockquote>
+      </div>
+      <div class="page">
+        <p>
+          Dengan Memohon Rahmat dan Ridho dari Allah SWT, Kami Bermasksud
+          Menyelenggarakan Pernikahan
+        </p>
+      </div>
+      <!-- page 4 -->
+      <div class="page">
+        <div class="col-6 col-md-3">
+          <div class="card bg-dark text-white" style="height: 300px;">
+            <img
+              src="../assets/cewe.jpg"
+              class="card-img img-fluid"
+              alt="..."
+              style="height: 100%; width: 100%; object-fit: cover;"
+            />
+            <div
+              class="card-img-overlay d-flex flex-column justify-content-end align-items-center text-wrap"
+            >
+              <p class="card-title">Risma Mahendra</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="page">
+        <div class="col-6 col-md-3">
+          <div class="card bg-dark text-white" style="height: 300px;">
+            <img
+              src="../assets/cowo.jpg"
+              class="card-img img-fluid"
+              alt="..."
+              style="height: 100%; width: 100%; object-fit: cover;"
+            />
+            <div
+              class="card-img-overlay d-flex flex-column justify-content-end align-items-center text-wrap"
+            >
+              <p class="card-title">Farhan Mahendra</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- page 5 -->
+      <div class="page">
+        <p class="d-inline-block me-1 ms-1">Love Story</p>
+      </div>
+      <div class="page"><p class="fw-bold mb-4">Perkenalan</p></div>
+      <!-- page 6 -->
+      <div class="page"><p class="fw-bold mb-4">Masa Pacaran</p></div>
+      <div class="page"><p class="fw-bold mb-4">Lamaran</p></div>
+      <!-- page 7 -->
+      <div class="page"><p class="fw-bold mb-4">Pernikahan</p></div>
+      <div class="page"></div>
+      <!-- page 8 -->
+      <div class="page"><p class="d-inline-block me-1 ms-1">Lokasi</p></div>
+      <div class="page">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d15845.029593698011!2d107.5150141!3d-6.85972325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sid!2sid!4v1711286546661!5m2!1sid!2sid"
+          style="width: 30vw;"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+        <p>
+          Jl. Cilame No.32, RT.05/ RW.04, Kecamatan Cilame, Kelurahan Ngamprah,
+          Kabupaten Bandung Barat
+        </p>
+      </div>
+      <!-- page 9 -->
+      <div class="page">
+        <p class="d-inline-block me-1 ms-1">Weeding Gift</p>
+      </div>
+      <div class="page">
+        <p>Atas Nama :</p>
+        <p>Farhan Mahendra</p>
+      </div>
+      <!-- page 10 -->
+      <div class="page"><p>Farhan & Risma Gallery</p></div>
+      <div class="page">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-4 col-md-12 mb-4 mb-lg-0">
+              <img
+                src="../assets/nikah1.jpg"
+                class="shadow-1-strong rounded mb-4"
+                alt="Boat on Calm Water"
+                style="height: 100px;"
+              />
+
+              <img
+                src="../assets/nikah2.jpg"
+                class="shadow-1-strong rounded mb-4"
+                alt="Wintry Mountain Landscape"
+                style="height: 100px;"
+              />
+
+              <img
+                src="../assets/nikah3.jpg"
+                class="shadow-1-strong rounded mb-4"
+                alt="Wintry Mountain Landscape"
+                style="height: 100px;"
+              />
+
+              <img
+                src="../assets/nikah4.jpg"
+                class="shadow-1-strong rounded mb-4"
+                alt="Wintry Mountain Landscape"
+                style="height: 100px;"
+              />
+
+              <img
+                src="../assets/nikah5.jpg"
+                class="shadow-1-strong rounded mb-4"
+                alt="Wintry Mountain Landscape"
+                style="height: 100px;"
+              />
+              <img
+                src="../assets/nikah6.jpg"
+                class="shadow-1-strong rounded mb-4"
+                alt="Wintry Mountain Landscape"
+                style="height: 100px;"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+        <!-- page 11 -->
+      <div class="page">
+        <p>Comment</p>
+        <div class="d-flex flex-start w-100">
+        <img
+          class="rounded-circle shadow-1-strong me-3"
+          src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(19).webp"
+          alt="avatar"
+          width="40"
+          height="40"
+        />
+        <div class="form-outline w-100">
+          <textarea
+            class="form-control"
+            id="textAreaExample"
+            rows="4"
+            style="background: #fff;"
+          ></textarea>
+        </div>
+      </div>
+      <div class="float-end mt-2 pt-1">
+        <div class="row">
+          <div class="col">
+            <button type="button" class="btn btn-primary btn-sm">Post</button>
+          </div>
+        </div>
+      </div>
+      </div>
+      <div class="page">
+        <div class="card-body p-4">
+              <div class="d-flex flex-start">
+                <img
+                  class="rounded-circle shadow-1-strong me-3"
+                  src="https://mdbcdn.b-cdn.net/img/Photos/Avatars/img%20(23).webp"
+                  alt="avatar"
+                  width="60"
+                  height="60"
+                />
+                <div>
+                  <h6 class="fw-bold mb-1">Maggie Marsh</h6>
+                  <div
+                    class="d-flex align-items-center justify-content-center mb-3 is-centered"
+                  >
+                    <p class="mb-0">
+                      March 07, 2021
+                      <span class="badge bg-success">Datang</span>
+                    </p>
+                  </div>
+                  <p class="mb-0">
+                    Lorem Ipsum is simply dummy text of the printing and
+                    typesetting industry. Lorem Ipsum has been the industry's
+                    standard dummy text ever since the 1500s, when an unknown
+                    printer took a galley of type and scrambled it.
+                  </p>
+                </div>
+              </div>
+            </div>
+      </div>
+        <!-- page 12 -->
+      <div class="page"></div>
+      <div class="page"></div>
+      <div class="page"></div>
+      <div class="page"></div>
+      <div class="page"></div>
+      <div class="page"></div>
+      <div class="page"></div>
+      <div class="page"></div>
+      <div class="page"></div>
+      <div class="page"></div>
+      <div class="page"></div>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      pages: new Array(32) // Membuat array dengan 32 elemen untuk mewakili halaman
-    };
-  },
-  mounted() {
-    this.arrangePages();
-  },
-  methods: {
-    arrangePages() {
-      const pages = document.getElementsByClassName('page');
-      for (let i = 0; i < pages.length; i++) {
-        const page = pages[i];
-        if (i % 2 === 0) {
-          page.style.zIndex = pages.length - i;
-        }
-      }
-    },
-    flipPage(index) {
-      const pages = document.getElementsByClassName('page');
-      const currentPage = pages[index];
-      const nextIndex = index + 1;
+<script setup>
+import { onMounted } from "vue";
 
-      if (nextIndex < pages.length) {
-        const nextPage = pages[nextIndex];
-        currentPage.classList.toggle('flipped');
-        nextPage.classList.toggle('flipped');
-      }
+onMounted(() => {
+  const pages = document.getElementsByClassName("page");
+
+  for (let i = 0; i < pages.length; i++) {
+    const page = pages[i];
+    if (i % 2 === 0) {
+      page.style.zIndex = pages.length - i;
     }
   }
-};
+
+  for (let i = 0; i < pages.length; i++) {
+    pages[i].pageNum = i + 1;
+    pages[i].onclick = function () {
+      if (this.pageNum % 2 === 0) {
+        this.classList.remove("flipped");
+        this.previousElementSibling.classList.remove("flipped");
+      } else {
+        this.classList.add("flipped");
+        this.nextElementSibling.classList.add("flipped");
+      }
+    };
+  }
+});
 </script>
